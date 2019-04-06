64c3e9810cf043454adbd917d75fffdb8bfe41f5
Kunal <kshah239@gmail.com>
2019-04-06 11:34:29 -0700

    styles directory

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@
 import React from "react";
 
 import Projects from "../components/Projects";
-import { Normalize } from "@smooth-ui/core-sc";
+import { Wrapper } from "../styles";
+import { Normalize, Grid, Row, Col } from "@smooth-ui/core-sc";
 import { Box, Card, Image, Heading, Text } from "rebass";
 
 import styled from "styled-components";
@@ -14,6 +15,7 @@ export default () => (
 
     <Section>
       <h1>Hey, I'm Kunal</h1>
+      {/* TODO: social icons  */}
       <p>I'm a full-stack developer based in San Francisco.</p>
       <p>kshah239@gmail.com</p>
     </Section>
@@ -22,43 +24,10 @@ export default () => (
       <Projects />
     </Section>
 
-    <Section>
-      footer
-      <Image src={"/static/images/logo.png"} style={{ height: "5em" }} />
-    </Section>
+    <Grid>
+      <Row justifyContent={{ xs: "center" }} py={5}>
+        <Image src={"/static/images/logo.png"} style={{ height: "3em" }} />
+      </Row>
+    </Grid>
   </Wrapper>
 );
-
-const Wrapper = styled.div`
-  body {
-    line-height: 1.6;
-    font-family: "Source Sans Pro", Helvetica, sans-serif;
-  }
-
-  p {
-    display: block;
-    margin-block-start: 1em;
-    margin-block-end: 1em;
-    margin-inline-start: 0px;
-    margin-inline-end: 0px;
-  }
-  a {
-    background-position: 0 0;
-    border-bottom: 2px solid;
-    border-color: rgba(85, 85, 85, 0.2);
-    box-sizing: border-box;
-    color: #555;
-    text-decoration: none;
-    transition-delay: 0s;
-    transition-duration: 0.15s;
-    transition-property: border-color;
-    transition-timing-function: ease-out;
-    :hover & {
-      border-color: rgba(85, 85, 85, 0.4);
-    }
-  }
-`;
-
-const Section = styled.section`
-  margin: 3em;
-`;
---
+++ styles/index.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+export const Wrapper = styled.div`
+  body {
+    line-height: 1.6;
+    font-family: "Source Sans Pro", Helvetica, sans-serif;
+  }
+
+  p {
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+  }
+  a {
+    background-position: 0 0;
+    border-bottom: 2px solid;
+    border-color: rgba(85, 85, 85, 0.2);
+    box-sizing: border-box;
+    color: #555;
+    text-decoration: none;
+    transition-delay: 0s;
+    transition-duration: 0.15s;
+    transition-property: border-color;
+    transition-timing-function: ease-out;
+    :hover & {
+      border-color: rgba(85, 85, 85, 0.4);
+    }
+  }
+`;
+
+const Section = styled.section`
+  margin: 3em;
+`;
