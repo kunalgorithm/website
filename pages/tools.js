c69acf93e4efd114af4f5c0c097f3cc28ff59ee9
Kunal <kshah239@gmail.com>
2019-04-22 20:52:28 -0700

    update /tools

--- pages/tools.js
+++ pages/tools.js
@@ -1,15 +1,7 @@
 import * as React from "react";
 
-import { Normalize, Grid, Row, Col } from "@smooth-ui/core-sc";
-import { Box, Card, Image, Heading, Text } from "rebass";
-import styled from "styled-components";
-import {} from "@smooth-ui/core-sc";
-import { Wrapper, Section } from "../styles";
-import Menu from "../components/Menu";
 //@ts-ignore
 import Tools from "../docs/tools.mdx";
-import Link from "next/link";
-import Footer from "../components/Footer";
 import Page from "../layouts/Page";
 
 export default () => (
