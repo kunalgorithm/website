8f1325648e72460c2b52b890560695bb4c3c6462
Kunal <me@kunal.sh>
2020-09-29 12:05:45 -0700

    update zoom link

--- src/pages/zoom.js
+++ src/pages/zoom.js
@@ -3,7 +3,9 @@ import Helmet from "react-helmet"
 
 export default ({}) => {
   if (typeof window !== `undefined`)
-    window.location.replace(`https://zoom.us/j/7865414444?pwd=OHRHUG1MeFZzNUtUTVRoN0Yxb2Z5dz09`)
+    window.location.replace(
+      `https://zoom.us/j/7865414444?pwd=VG5GeVIyaVE2VWhWUmI5RHZqbkFXdz09`
+    )
   return (
     <div>
       <Helmet title="Join My Personal Zoom Meeting"></Helmet>
