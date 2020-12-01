cc3311e65c73d3577840f75a43449601d94ad7f4
Kunal Shah <kunal@kunals-mbp.lan>
2020-12-01 10:55:42 -0500

    re-enable the redirect

--- src/pages/zoom.js
+++ src/pages/zoom.js
@@ -1,12 +1,11 @@
 import React from "react"
 import Helmet from "react-helmet"
-import { Row, Col } from "antd"
 
 export default ({}) => {
-  // if (typeof window !== `undefined`)
-  //   window.location.replace(
-  //     `https://zoom.us/j/5962714465?pwd=MUxiSFBxOTNBS2I3OGl5NldZeDdJdz09`
-  //   )
+  if (typeof window !== `undefined`)
+    window.location.replace(
+      `https://zoom.us/j/5962714465?pwd=MUxiSFBxOTNBS2I3OGl5NldZeDdJdz09`
+    )
   return (
     <div>
       <Helmet title="Join My Personal Zoom Meeting"></Helmet>
