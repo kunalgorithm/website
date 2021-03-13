bb1d35190d41cdb179596aebbce0eb7e1dad643b
Kunal Shah <me@kunal.sh>
2021-03-13 09:12:46 +0800

    delete everything from old site

--- src/pages/odf.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-import { Redirect } from "@reach/router"
-
-export default ({}) => {
-  if (typeof window !== `undefined`) window.location.replace(`/posts/odf`)
-  return <Redirect to="/posts/odf" />
-}
