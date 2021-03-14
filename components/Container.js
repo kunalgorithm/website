f5ff689a8ebe80e3a877992ce644e545e2453b6d
Kunal Shah <me@kunal.sh>
2021-03-14 16:53:05 +0800

    custom dark mode icon and code highlighting

--- components/Container.js
+++ components/Container.js
@@ -85,28 +85,16 @@ export default function Container(props) {
             >
               {theme === 'dark' ? (
                 <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
                   d="M10 18.75V1.25a8.75 8.75 0 100 17.5zM10 20a10 10 0 100-20 10 10 0 000 20z"
                 ></path>
               ) : (
-                // <path
-                //   strokeLinecap="round"
-                //   strokeLinejoin="round"
-                //   strokeWidth={2}
-                //   d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                // />
                 <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
                   d="M10 18.75V1.25a8.75 8.75 0 100 17.5zM10 20a10 10 0 100-20 10 10 0 000 20z"
                 ></path>
-                // <path
-                //   strokeLinecap="round"
-                //   strokeLinejoin="round"
-                //   strokeWidth={2}
-                //   d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                // />
               )}
             </svg>
           )}
