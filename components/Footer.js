1eac6253d04a348c0787ba83df1f419f2dc2fb8e
Kunal Shah <me@kunal.sh>
2021-11-06 14:57:23 -0400

    new twitter handle

--- components/Footer.js
+++ components/Footer.js
@@ -33,7 +33,7 @@ export default function Footer() {
           </Link>
         </div>
         <div className="flex flex-col space-y-6">
-          <ExternalLink href="https://twitter.com/kunalgorithm">
+          <ExternalLink href="https://twitter.com/kunalshxh">
             Twitter
           </ExternalLink>
           <ExternalLink href="https://github.com/kunalgorithm">
