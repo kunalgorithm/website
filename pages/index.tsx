48b22e64acbb51d556d6ef2951eaa73df4470876
Kunal Shah <me@kunal.sh>
2025-07-12 19:58:02 -0700

    update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,8 +16,49 @@ export default function Home({ posts }) {
     <Container>
       <div className="flex flex-col justify-center items-start max-w-2xl w-full mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-10 ">
-          Hey, I’m Kunal Shah
+          Hey, I'm Kunal Shah
         </h1>
+
+        <div className="prose prose-lg mb-8 leading-6">
+          <p className="mb-4">
+            I'm the founder of Cocomo - we help companies improve marketing
+            using autonomous media buying and conversion rate optimization.
+            We're currently in stealth, but I'd be happy to share more on a
+            call.
+          </p>
+
+          <p className="mb-4">Here's some of what I've done in the past:</p>
+
+          <ul className="mb-4 ml-6 list-disc">
+            <li>
+              founded compose.ai, where i built an ai-powered email writer
+              (before LLMs exploded in popularity), joined YC's w21 batch, and
+              scaled to 500k monthly active users.
+            </li>
+            <li>
+              founded Ascend Motors, where i built electric motorcycles with
+              200-mile range using breakthrough battery pack configurations and
+              cooling technologies.
+            </li>
+            <li>
+              founding team at On Deck, where i grew into the head of
+              engineering and helped bring the founders fellowship to life.
+            </li>
+            <li>
+              studied CS, ran D1 track, and started the blockchain club at Rice
+            </li>
+            <li>
+              in high school, i held the New York State record in the indoor
+              mile (4:16, if you must know), and was voted best dressed
+            </li>
+          </ul>
+
+          <p className="mb-6">
+            I'm enthusiastic about cars, motorcycles, drones, video games, art
+            (yes even AI art), political theory, philosophy, and residential
+            architecture.
+          </p>
+        </div>
 
         {allPosts.map((frontMatter) => (
           <BlogPostPreview key={frontMatter.title} {...frontMatter} />
